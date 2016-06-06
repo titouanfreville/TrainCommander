b9e1832fd4bfacbb1217d99b6d0592347cef727f
titouanfreville <titouanfreville@hotmail.fr>
2016-06-06 13:33:08 +0200

    Pass through env_var in dockers

--- web_app/ia_api/firstime.scala
+++ web_app/ia_api/firstime.scala
@@ -9,8 +9,16 @@ object Listes {
       case x::tails => x==e || find_in_int(tails,e)
     }
   }
-
+  def split_list(l:List[Int]):(List[Int],List[Int]) = {
+    l match {
+      case Nil => (Nil,Nil)
+      case x::Nil =>  (x,Nil)
+      case a::b::tails => var (l1,l2)=split_list(tails);  (a::l1,b::l2)
+    }
+  }
 }
 
 Listes.find_in_int(Listes.l1,2)
 Listes.find_in_int(Listes.l1,10)
+Liste.split_list(Listes.l1)
+Liste.split_list(Listes.l2)
