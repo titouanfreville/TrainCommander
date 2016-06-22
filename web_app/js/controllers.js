a7cffce3273664c1bff069b51fe0fe4e92a83097
titouanfreville <titouanfreville@hotmail.fr>
2016-06-22 18:50:11 +0200

    Proper Login Controller.

--- web_app/js/controllers.js
+++ web_app/js/controllers.js
@@ -20,36 +20,44 @@
 
 var $module = angular.module('rtt', ["apiServices", "ngCookies"]);
 $module.controller('LoginController', ['$scope', '$window', '$cookies', 'DbUser', function($scope, $window , $cookies, DbUser) {
-  $scope.mail = "marsu@pilami.palombia";
-  $scope.password = "HoubaBahou";
+  // $scope.mail = "marsu@pilami.palombia";
+  // $scope.password = "HoubaBahou";
   $scope.submit = function() {
-    var user
+    var user;
+    var CredError = "Les identifiants renseigner sont incorrect. Vérifier que vous avez bien saisie votre mot de passe et votre email utiliser lors de l'enregistrement."
+    var CredNotProvided = "Email ou Passowrd non renseigner. Merci de remplir ces champs afin de pouvoir accéder à votre espace."
     if ($scope.mail && $scope.password) {
       DbUser.findOne(
         {filter: 
           {
           where: {email: $scope.mail} 
           } 
-        }).$promise.then(function(results) {
-          user=results.toJSON();
-
+        },
+        function(value, responseHeaders) {
+          user=value.toJSON();
           if ( angular.isUndefined(user) ) { }
           else {
             if ($scope.password == user.password) {
               $cookies.put('user', user.id);
               $window.location.href = '/index.html';
             }
+            else {
+              $window.alert(CredError)
+            }
           }
-
+        },
+        function error(httpResponse) {
+          var status = httpResponse.status;
+          if (status == 404) {
+            $window.alert(CredError)
+          }
         })
-
-      
     }
     else {
-      $window.alert("Email ou Passowrd non renseigner. Merci de remplir ces champs afin de pouvoir accéder à votre espace.")
+      $window.alert(CredNotProvided)
     }
   };
 }]);
 $module.controller('test',[], function($scope) {
-
+  
 })
