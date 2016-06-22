ec395a9455b9833f75e4eee375036f46c41aa0f4
titouanfreville <titouanfreville@hotmail.fr>
2016-06-22 18:09:57 +0200

    Login Working without delay :p

--- web_app/js/controllers.js
+++ web_app/js/controllers.js
@@ -1,18 +1,18 @@
 /**
- * @ngdoc controller
- * @name IndexCtrl
- * @author Christophe Eble <ceble@nexway.com>
- * @author Titouan Freville <tfreville@nexway.com>
- * @module iab
- * @description
- * Main controller for InApp Billing
- *
- * @usage
- *
- * ```html
- * <body ng-controller="MainCtrl"></body>
- * ```
- */
+* @ngdoc controller
+* @name IndexCtrl
+* @author Christophe Eble <ceble@nexway.com>
+* @author Titouan Freville <tfreville@nexway.com>
+* @module iab
+* @description
+* Main controller for InApp Billing
+*
+* @usage
+*
+* ```html
+* <body ng-controller="MainCtrl"></body>
+* ```
+*/
 
 /* jshint +W097 */
 'use strict';
@@ -20,26 +20,30 @@
 
 var $module = angular.module('rtt', ["apiServices", "ngCookies"]);
 $module.controller('LoginController', ['$scope', '$window', '$cookies', 'DbUser', function($scope, $window , $cookies, DbUser) {
-  // $scope.list = [];
-  // $scope.text = 'hello';
   $scope.mail = "marsu@pilami.palombia";
   $scope.password = "HoubaBahou";
-  var user;
   $scope.submit = function() {
+    var user
     if ($scope.mail && $scope.password) {
-      
       DbUser.findOne(
         {filter: 
           {
-            where: {email: $scope.mail} 
+          where: {email: $scope.mail} 
           } 
         }).$promise.then(function(results) {
-          user = results.toJSON();
-        });
-      if ($scope.password == user.password) {
-        $cookies.put('user', user.id);
-        $window.location.href = '/index.html';
-      }
+          user=results.toJSON();
+
+          if ( angular.isUndefined(user) ) { }
+          else {
+            if ($scope.password == user.password) {
+              $cookies.put('user', user.id);
+              $window.location.href = '/index.html';
+            }
+          }
+
+        })
+
+      
     }
     else {
       $window.alert("Email ou Passowrd non renseigner. Merci de remplir ces champs afin de pouvoir accéder à votre espace.")
